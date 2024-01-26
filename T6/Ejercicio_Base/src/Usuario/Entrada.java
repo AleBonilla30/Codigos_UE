1f8fe30291bc6e644602665f41e34e84a8f34010
Alejandra Bonilla <ale.bonilla309@gmail.com>
2024-01-26 16:55:39 +0100

    ejerciciosBase

---
+++ T6/Ejercicio_Base/src/Usuario/Entrada.java
@@ -0,0 +1,69 @@
+package Usuario;
+
+import java.util.Scanner;
+
+public class Entrada {
+    public static void main(String[] args) {
+        String nombre, apellido, dni;
+        int edad;
+        double altura, peso;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el nombre: ");
+        nombre = scanner.next();
+        System.out.print("Ingrese el apellido: ");
+        apellido = scanner.next();
+        System.out.print("Ingrese el DNI: ");
+        dni = scanner.next();
+        System.out.print("Ingrese la edad: ");
+        edad = scanner.nextInt();
+        System.out.print("Ingrese el peso: ");
+        peso = scanner.nextDouble();
+        System.out.print("Ingrese la altura: ");
+        altura = scanner.nextDouble();
+
+        Persona p1= new Persona(nombre, apellido, dni,edad,altura,peso);
+        Persona p2 = new Persona("Claudia", "Perez");
+        Persona p3 = new Persona();
+        System.out.println("Dato de la P1:");
+        p1.mostrarDato();
+        System.out.println("Dato de la P2:");
+        p2.mostrarDato();
+        System.out.println("Dato de la P3:");
+        p3.mostrarDato();
+
+        p1.setEdad(20);
+        p2.setEdad(20);
+        p3.setEdad(20);
+        System.out.println("Los datos actualizados de P1:");
+        p1.mostrarDato();
+        System.out.println("Los datos actualizados de P2:");
+        p2.mostrarDato();
+        System.out.println("Los datos actualizados de P3:");
+        p3.mostrarDato();
+        p1.incrementarEdad(5);
+        System.out.println("Los datos actualizados por segunda vez de P1:");
+        p1.mostrarDato();
+
+        double imcp1= p1.calcularIMC();
+        System.out.println("IMC de P1 es: "+imcp1);
+
+        mostrarEstado(imcp1);
+
+
+    }
+    public static void mostrarEstado(double imc){
+        System.out.println("-----Estado Fisico-----");
+        if (imc < 18.5){
+            System.out.println("Peso inferior al normal");
+        } else if (imc >= 18.5 && imc <24.9) {
+            System.out.println("Peso Normal");
+        } else if (imc >= 25 && imc <29.9) {
+            System.out.println("Peso superior a lo normal");
+        }else {
+            System.out.println("Obesidad");
+        }
+    }
+
+    }
+
---
+++ T6/Ejercicio_Base/src/Usuario/Persona.java
@@ -0,0 +1,98 @@
+package Usuario;
+
+public class Persona {
+    private String nombre, apellido, dni;
+    private int edad;
+    private double peso, altura;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, String dni, int edad, double altura, double peso) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.edad = edad;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public Persona(String nombre, String apellido, String dni, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.edad = edad;
+    }
+
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = "111111111X";
+        this.altura= 0;
+        this.peso= 0.0;
+        this.edad= 0;
+    }
+    public void mostrarDato(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+        System.out.println("DNI: "+dni);
+        System.out.println("Edad: "+edad);
+        System.out.println("Peso: "+peso);
+        System.out.println("Altura: "+altura);
+        System.out.println();
+    }
+    public void incrementarEdad(int incremento){
+        this.edad += incremento;
+    }
+    public double calcularIMC(){
+        return peso/(altura*altura);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+}
