f286a62a35bb64025baede6af70114e32f720d1b
Alejandra Bonilla <ale.bonilla309@gmail.com>
2024-05-21 18:06:10 +0200

    Update Entrada.java

--- Ejercicios Examen 2/Ejercicio3/src/Entrada.java
+++ Ejercicios Examen 2/Ejercicio3/src/Entrada.java
@@ -6,17 +6,32 @@ public class Entrada {
         System.out.println("Introduce un palabra sin acento:");
         String palabra = scanner.nextLine();
 
-        int longitud = palabra.length();
 
-        if (longitud < 4){
+
+        if (palabra.length() < 4){
             String mayuscula = palabra.toUpperCase();
             System.out.println(mayuscula);
 
-        } else if (longitud >= 4 && longitud < 8) {
+        } else if (palabra.length() >= 4 && palabra.length() < 8) {
             String minuscula = palabra.toLowerCase();
             System.out.println(minuscula);
 
-        }else if (longitud > 11){
+        } else if (palabra.length() >= 8 && palabra.length() <11) {
+            String replaced = palabra
+                    .replace('a','á')
+                    .replace('e','é')
+                    .replace('i','í')
+                    .replace('o','ó')
+                    .replace('u','ú')
+                    .replace('A','Á')
+                    .replace('E','É')
+                    .replace('I','Í')
+                    .replace('O','Ó')
+                    .replace('U','Ú');
+            System.out.println("Acento en todas las vocales: "+replaced);
+
+            
+        } else if (palabra.length() > 11){
          StringBuilder reversa = new StringBuilder(palabra).reverse();
             System.out.println(reversa);
         }
